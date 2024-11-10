817ae003908d2dd48be03dc63ae69a44ce79dc6b
official4jelas <ydiskyha@gmail.com>
2024-11-10 11:20:29 +0700

    Create owner-sf.js

---
+++ plugins/owner-sf.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+
+const handler = async (m, { text, usedPrefix, command }) => {
+    if (!text) throw `uhm.. teksnya mana?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} plugins/menu.js`;
+    if (!m.quoted.text) throw `balas pesan nya!`;
+    let path = `${text}`;
+    await fs.writeFileSync(path, m.quoted.text);
+    m.reply(`tersimpan di ${path}`);
+}
+
+handler.help = ['sf'].map(v => v + ' <teks>');
+handler.tags = ['owner'];
+handler.command = /^sf$/i;
+
+handler.owner = true;
+
+export default handler;
